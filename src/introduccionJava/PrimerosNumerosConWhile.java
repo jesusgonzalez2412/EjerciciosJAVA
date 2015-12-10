62a5f86f15599f980b5b33f98e53259fd6db7c60
jesusgonzalez2412 <jesusgonzalez2412@gmail.com>
2015-12-10 02:10:56 -0600

    Demostración de la estructura repititiva While

---
+++ src/introduccionJava/PrimerosNumerosConWhile.java
@@ -0,0 +1,27 @@
+
+package introduccionJava;
+import java.util.Scanner;
+/**
+ *Programa que utiliza la estructura iterativa While para mostrar los primeros
+ * N numeros naturales. El usuario ingresa el valor N por teclado
+ * @author RootX0d4y
+ */
+public class PrimerosNumerosConWhile {
+    
+    public static void main(String[]args )
+    {
+        int contador=1, n=0;
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese la cantidad de numero que desea imprimir");
+        n = scanner.nextInt();
+        
+        while(contador <= n){
+            //Muestra el valor de contador
+            System.out.println("El valor es: "+contador);
+            //Incrementamos el valor del contador
+            contador++;
+        }
+    }
+    
+}
